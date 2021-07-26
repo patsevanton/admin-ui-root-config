e90f8d2aa086f164fe5fcde3ba8da2eebe382a03
Anton Patsev <patsev.anton@gmail.com>
2021-07-26 15:48:19 +0600

    EPMDJ-8123 Configure actions and publish for microfronts

--- src/ui/containers-paths.ts
+++ src/ui/containers-paths.ts
@@ -17,5 +17,5 @@
 export const paths = {
   test2code: process.env.NODE_ENV === "development"
     ? "http://localhost:8080/Drill4J-test-to-code.js"
-    : "https://cdn.jsdelivr.net/npm/@drill4j/test2code-ui@0.1.1/dist/Drill4J-test-to-code.js",
+    : "https://cdn.jsdelivr.net/npm/@drill4j/test2code-ui@latest/dist/Drill4J-test-to-code.js",
 };

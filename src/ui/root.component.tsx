4495433c40130448c373ecdf75f460129e116565
Ilia_Solovev1 <49729985+IliaSolovev@users.noreply.github.com>
2021-08-03 14:06:15 +0300

    fix: incorrect search input styles (#24)

--- src/ui/root.component.tsx
+++ src/ui/root.component.tsx
@@ -30,6 +30,7 @@ import { AppLayout } from "layouts";
 import { NotificationManager } from "./notification-manager";
 
 import "./index.css";
+import "../../node_modules/@drill4j/ui-kit/dist/tailwind.css";
 
 configureAxios();
 

7dbf8818df6ef6fb3ce0eafef144ca311e2344be
Ilia_Solovev1 <49729985+IliaSolovev@users.noreply.github.com>
2021-10-07 15:30:22 +0300

    feat: add "add-agent" panel

--- src/ui/components/panels/add-agent-panel.tsx
+++ src/ui/components/panels/add-agent-panel.tsx
@@ -13,12 +13,113 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import React from "react";
-import { Panel } from "./panel";
+import React, { useState } from "react";
+import tw, { styled } from "twin.macro";
+import { Button, Icons } from "@drill4j/ui-kit";
+import { PanelWithCloseIcon } from "./panel-with-close-icon";
 import { PanelProps } from "./panel-props";
+import { useAdminConnection } from "../../hooks";
+import { Agent, ServiceGroup } from "../../types";
+import { AGENT_STATUS } from "../../common";
+import { useSetPanelContext } from "./panel-context";
 
-export const AddAgentPanel = ({ isOpen, onClosePanel }: PanelProps) => (
-  <Panel header={<div>Add Agent</div>} isOpen={isOpen} onClosePanel={onClosePanel}>
-    <div style={{ width: "1100px" }} />
-  </Panel>
-);
+export const AddAgentPanel = ({ isOpen, onClosePanel }: PanelProps) => {
+  const agentsList = useAdminConnection<Agent[]>("/api/agents") || [];
+  const groupsList = useAdminConnection<ServiceGroup[]>("/api/groups") || [];
+  const agents = agentsList.filter((agent) => !agent.group && agent.status === AGENT_STATUS.NOT_REGISTERED);
+  const groupsAgents = agentsList.filter((agent) => agent.group && agent.status === AGENT_STATUS.NOT_REGISTERED);
+  const groups = groupsList.map((group) => ({
+    group,
+    agents: groupsAgents.filter((agent) => group.name === agent.group),
+  }));
+
+  return (
+    <PanelWithCloseIcon header={<div tw="w-[1024px] flex items-center h-20">Add Agent</div>} isOpen={isOpen} onClosePanel={onClosePanel}>
+      <Layout tw="text-monochrome-dark font-bold text-14 leading-24">
+        <Column tw="col-start-2">Name</Column>
+        <Column tw="col-start-3">Type</Column>
+      </Layout>
+      <div tw="flex flex-col gap-y-[6px] overflow-y-auto text-monochrome-dark-tint">
+        {groups.map(({ group, agents: groupAgents }) => groupAgents.length > 0
+          && <GroupRow key={group?.id} group={group} agents={groupAgents} />)}
+        {agents.map((agent) => <AgentRow key={agent.id} {...agent} />)}
+      </div>
+    </PanelWithCloseIcon>
+  );
+};
+
+interface GroupRowProps {
+  agents: Agent[];
+  group: ServiceGroup;
+}
+
+const Layout = styled.div`
+  ${tw`grid items-center grid-cols-[28px 3fr 1fr 104px] h-[60px] px-4`}
+`;
+
+const Column = styled.div`
+  ${tw`px-3 text-ellipsis`}
+`;
+
+const GroupRow = ({ group: { name }, agents }:GroupRowProps) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <div tw="rounded-lg border border-monochrome-dark bg-monochrome-black text-monochrome-dark-tint text-14 leading-20">
+      <Layout css={[tw`border-monochrome-dark`, isOpen && tw`border-b`]}>
+        <div
+          tw="flex items-center justify-center cursor-pointer"
+          onClick={() => setIsOpen(!isOpen)}
+        >
+          <Icons.Expander
+            rotate={isOpen ? 90 : 0}
+            width={9}
+            height={16}
+          />
+        </div>
+        <Column title={name}>{name}</Column>
+        <Column title="Multiservice">Multiservice</Column>
+        <Button
+          primary
+          size="small"
+        ><Icons.Register width={16} height={16} /> Register
+        </Button>
+      </Layout>
+      {isOpen && agents.map((agent) => <GroupAgentRow key={agent.id} {...agent} />)}
+    </div>
+  );
+};
+
+const AgentRow = ({ name, agentType, id }:Agent) => {
+  const setPanel = useSetPanelContext();
+
+  return (
+    <Layout tw="rounded-lg bg-monochrome-black box-border border border-monochrome-dark text-monochrome-dark-tint text-14 leading-20">
+      <Column tw="col-start-2" title={name}>{name}</Column>
+      <Column title={agentType}>{agentType}</Column>
+      <Button
+        onClick={() => setPanel({ type: agentType === "Java" ? "JAVA_AGENT_REGISTRATION" : "JS_AGENT_REGISTRATION", payload: id })}
+        primary
+        size="small"
+      >
+        <Icons.Register width={16} height={16} /> Register
+      </Button>
+    </Layout>
+  );
+};
+
+const GroupAgentRow = ({ id, name, agentType }:Agent) => {
+  const setPanel = useSetPanelContext();
+  return (
+    <Layout>
+      <Column tw="col-start-2" title={name}>{name}</Column>
+      <Column title={agentType}>{agentType}</Column>
+      <Button
+        onClick={() => setPanel({ type: agentType === "Java" ? "JAVA_AGENT_REGISTRATION" : "JS_AGENT_REGISTRATION", payload: id })}
+        secondary
+        size="small"
+      ><Icons.Register width={16} height={16} /> Register
+      </Button>
+    </Layout>
+  );
+};
